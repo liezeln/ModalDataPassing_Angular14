71ce09ce37a902639eee7b4efd9b7932d3b3e3a4
Liezel Nel <liezeln@gmail.com>
2023-04-19 22:27:58 +0200

    Final application

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ItemAddModalComponent } from '../modals/item-add-modal/item-add-modal.component';
 
 @Component({
   selector: 'app-item-list',
@@ -7,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
   itemList = ["Book", "Pen"];
+  bsModalRef: BsModalRef | undefined;
   
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
   }
 
+  openModalWithComponent() {
+    const config = {
+      class: 'modal-dialog-centered',
+      initialState: {
+        numberOfItems: this.itemList.length,
+      }
+    };
+    this.bsModalRef = this.modalService.show(ItemAddModalComponent, config);
+    this.bsModalRef.content.addNewItem.subscribe({
+      next: (newItem: string) => this.itemList.push(newItem)
+    })
+  }
 }
--- src/app/modals/item-add-modal/item-add-modal.component.ts
+++ src/app/modals/item-add-modal/item-add-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-item-add-modal',
@@ -9,9 +10,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ItemAddModalComponent implements OnInit {
   itemForm: FormGroup;
   numberOfItems = 0;
+  list: any[] = [];
+  addNewItem = new EventEmitter();
 
-
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, public bsModalRef: BsModalRef) {
     this.itemForm = this.formBuilder.group({
       name: ""
     });
@@ -21,7 +23,11 @@ export class ItemAddModalComponent implements OnInit {
   }
 
   saveToList() {
-    console.log(this.itemForm.value)
+    let newItem = this.itemForm.value.name;
+    if (newItem != "") {
+      this.addNewItem.emit(newItem);
+    }
+    this.bsModalRef.hide();
  }
 
 }
